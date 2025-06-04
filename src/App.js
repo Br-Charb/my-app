00f1dd4bac3f41621c9e49b8b3b4b928acde51d9
Ben Charbonneau <benjamincharbonneau@icloud.com>
2025-06-04 02:23:01 -0400

    Initialize repo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Title-row">
+          <img src='/images/octagon_left.png' className="App-logo App-logo-left" alt="octagon_left" />
+          <h1 className='App-title'>OCTADLE</h1>
+          <img src="/images/octagon_right.png" className="App-logo App-logo-right" alt="octagon_right" />
+        </div>
+        <div className="answer-row">
+          
+        </div>
       </header>
     </div>
   );
