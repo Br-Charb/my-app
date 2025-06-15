a51cc8357ab1cdfcd80c00cee06b61ae191d0faa
Ben Charbonneau <benjamincharbonneau@icloud.com>
2025-06-15 02:47:24 -0400

    Added dropdown suggestions for fighter names

--- src/components/GuessBar.js
+++ src/components/GuessBar.js
@@ -4,31 +4,61 @@ export const GuessBar = ({ checkResponse, todaysFighter, possibleFighters }) => 
 
     const [inputValue, setInputValue] = useState('');
     const [fighter, setFighter] = useState(null);
+    const [isOpen, setIsOpen] = useState(false);
 
     const returnFighterInfo = ((fighterName) => {
         return possibleFighters.find(possibleFighters => possibleFighters.Fighter.toLowerCase() === fighterName.toLowerCase());
     })
+
+    const checkNameSimilarity = ((text, fighterName) => {
+        const firstName = fighterName.startsWith(text);
+        const lastName = fighterName.split(" ").slice(-1)[0].startsWith(text);
+        const emptySearch = text !== "";
+        return (firstName || lastName) && emptySearch;
+    })
+
+    const submitFighter = ((fighter) => {
+        console.log(fighter);
+        checkResponse(fighter);
+        setInputValue('');
+    })
     
     return (
-        <input type="text"
-            className="Guess-prompt" 
-            placeholder="Guess"
-            value={inputValue}
-            onChange={(e) => {
-                console.log(inputValue);
-                setInputValue(e.target.value);
-            }
-            }
-            onKeyDown={(e) => {
-                if (e.key === "Enter"){
-                    let fighter = returnFighterInfo(inputValue);
-                    if (fighter !== undefined) {
-                        checkResponse(fighter);
-                        setInputValue('');
-                        fighter = null;
+        
+        <>
+            <div>
+                <input type="text"
+                    className="Guess-prompt" 
+                    placeholder="Guess"
+                    value={inputValue}
+
+                    onFocus={() => setIsOpen(true)}
+                    onBlur={() => setTimeout(() => setIsOpen(false), 100)}
+
+                    onChange={(e) => {
+                        console.log(inputValue);
+                        setInputValue(e.target.value);
                     }
-                }
-            }}
-        />
+                    }
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter"){
+                            let fighter = returnFighterInfo(inputValue);
+                            if (fighter !== undefined) {
+                                submitFighter(fighter);
+                                fighter = null;
+                            }
+                        }
+                    }}
+                />
+            </div>
+            {<div className={`Guess-options ${isOpen ? "open" : "close"}`}>
+                {Array.isArray(possibleFighters) &&
+                    possibleFighters.filter(item => checkNameSimilarity(inputValue.toLowerCase(), item.Fighter.toLowerCase())).map((item) => (
+                    <div className='Individual-guess' onClick={() => submitFighter(item)}>
+                        {item.Fighter}
+                    </div>
+                ))}
+            </div>}
+        </>
     );
 }
