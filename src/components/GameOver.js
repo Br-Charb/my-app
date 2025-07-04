637e80c6c90844437d45eb144ec565e1aa0927a2
Ben Charbonneau <benjamincharbonneau@icloud.com>
2025-07-03 20:08:15 -0400

    Corrected alignment of the last card section of the GameOver component to make it more vertically center

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -22,43 +22,44 @@ export const GameOver = (({ fighter, guesses, silhoutteViewable}) => {
                         <p className="Popup-name-upper">Todays Fighter...</p>
                         <p className="Popup-name-lower">{showName(silhoutteViewable, fighter)}</p>
                     </div>
-                    {silhoutteViewable && <div className="Popup-info open">
-                        <div className="Popup-stat-left">
-                            <div className="Popup-stat-stack">
-                                <div className="Popup-stat">
-                                    <div className="Popup-stat-heading">Nationality</div>                                    <div><img src={`/images/flags/${fighter.Nationality.split(" ").join("")}.png`} className="Popup-flag" alt={`${fighter.Nationality} Flag`} /></div>
+                    {silhoutteViewable && 
+                        <div className="Popup-info open">
+                            <div className="Popup-stat-left">
+                                <div className="Popup-stat-stack">
+                                    <div className="Popup-stat">
+                                        <div className="Popup-stat-heading">Nationality</div>                                    <div><img src={`/images/flags/${fighter.Nationality.split(" ").join("")}.png`} className="Popup-flag" alt={`${fighter.Nationality} Flag`} /></div>
+                                    </div>
+                                    <div className="Popup-stat">
+                                        <div className="Popup-stat-heading">Age</div>
+                                        <p className="Popup-stat-text">{Math.floor((Date.now() - (new Date(fighter.Age)))/31622400000)}</p>
+                                    </div>
+                                    <div className="Popup-stat">
+                                        <div className="Popup-stat-heading">Weight</div>
+                                        <div className="Popup-stat-text">{fighter.Weight}</div>
+                                    </div>
                                 </div>
-                                <div className="Popup-stat">
-                                    <div className="Popup-stat-heading">Age</div>
-                                    <p className="Popup-stat-text">{Math.floor((Date.now() - (new Date(fighter.Age)))/31622400000)}</p>
-                                </div>
-                                <div className="Popup-stat">
-                                    <div className="Popup-stat-heading">Weight</div>
-                                    <div className="Popup-stat-text">{fighter.Weight}</div>
+                                <div className="Popup-stat-stack">
+                                    <div className="Popup-stat">
+                                        <div className="Popup-stat-heading">Peak Rank</div>
+                                        <p className="Popup-stat-text">{fighter.PeakRank}</p>
+                                    </div>
+                                    <div className="Popup-stat">
+                                        <div className="Popup-stat-heading">Height</div>
+                                        <p className="Popup-stat-text">{fighter.Height}"</p>
+                                    </div>
+                                    <div className="Popup-stat">
+                                        <div className="Popup-stat-heading">Wins</div>
+                                        <p className="Popup-stat-text">{fighter.Record.split("-")[0]}</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="Popup-stat-stack">
+                            <div className="Popup-stat-right">
                                 <div className="Popup-stat">
-                                    <div className="Popup-stat-heading">Peak Rank</div>
-                                    <p className="Popup-stat-text">{fighter.PeakRank}</p>
-                                </div>
-                                <div className="Popup-stat">
-                                    <div className="Popup-stat-heading">Height</div>
-                                    <p className="Popup-stat-text">{fighter.Height}"</p>
-                                </div>
-                                <div className="Popup-stat">
-                                    <div className="Popup-stat-heading">Wins</div>
-                                    <p className="Popup-stat-text">{fighter.Record.split("-")[0]}</p>
+                                    <div className="Popup-stat-heading-right">Last Card</div>
+                                    <p className="Popup-stat-text Popup-right-height">{fighter.LastFight}</p>
                                 </div>
                             </div>
-                        </div>
-                        <div className="Popup-stat-right">
-                            <div className="Popup-stat">
-                                <div className="Popup-stat-heading-right">Last Card</div>
-                                <p className="Popup-stat-text Popup-right-height">{fighter.LastFight}</p>
-                            </div>
-                        </div>
-                    </div> }
+                        </div> }
                 </div>
             </div>
     )
